39c08658e653ec7d1dd2aa1c7548e2ff4d9270a9
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-26 23:42:44 +0600

    Add Search bar.

--- src/containers/SkyscrapersList/index.js
+++ src/containers/SkyscrapersList/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import List from './List';
+import Search from './Search';
+import { Divider } from 'semantic-ui-react';
 
 const SkyscrapersListComponent = () => (
-  <List />
+  <div>
+    <Search />
+    <Divider hidden />
+    <List />
+  </div>
 );
 
 export default SkyscrapersListComponent;
