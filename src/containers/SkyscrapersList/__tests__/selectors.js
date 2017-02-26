ed586ef937894f420781f9c289e9fd4ce447e867
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-26 23:59:08 +0600

    Add selector for `state.skyscrapers.loading` property.

--- src/containers/SkyscrapersList/__tests__/selectors.js
+++ src/containers/SkyscrapersList/__tests__/selectors.js
@@ -1,12 +1,23 @@
 import { fromJS } from 'immutable';
-import { selectSkyscrapers } from '../selectors';
+import {
+  selectSkyscrapers,
+  selectLoading,
+} from '../selectors';
 
-const skyscrapers = 'skyscrapers/SkyscrapersList/skyscrapers';
+const loading = 'skyscrapers/SkyscrapersList/loading';
+
+const skyscrapers = {
+  loading
+};
 
 const state = fromJS({
   skyscrapers,
 })
 
 test('selectSkyscrapers() selects [`skyscrapers`]', () => {
-  expect(selectSkyscrapers()(state)).toBe(skyscrapers);
+  expect(selectSkyscrapers()(state).toJS()).toEqual(skyscrapers);
+});
+
+test('selectLoading() selects [`skyscrapers`, `loading`]', () => {
+  expect(selectLoading()(state)).toBe(loading);
 });
--- src/containers/SkyscrapersList/selectors.js
+++ src/containers/SkyscrapersList/selectors.js
@@ -1,1 +1,8 @@
+import { createSelector } from 'reselect';
+
 export const selectSkyscrapers = () => state => state.get('skyscrapers');
+
+export const selectLoading = () => createSelector(
+  selectSkyscrapers(),
+  skyscrapers => skyscrapers.get('loading'),
+);
