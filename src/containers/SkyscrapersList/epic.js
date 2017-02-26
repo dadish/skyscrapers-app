85c5777986e4dbc320d92cc3910c07fab48f5837
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-26 09:11:52 +0600

    Implement search epic for SkyscrapersList.

---
+++ src/containers/SkyscrapersList/epic.js
@@ -0,0 +1,52 @@
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/switchMap';
+import { concat as concat$ } from 'rxjs/observable/concat';
+import { of as of$ } from 'rxjs/observable/of';
+import { ajax } from 'rxjs/observable/dom/ajax';
+import {
+  CHANGE_SEARCH_TXT,
+} from './constants';
+import {
+  ajaxSearchStart,
+  ajaxSearchEnd,
+  ajaxSearchFail,
+} from './actions';
+
+/**
+ * SkyscrapersList epic
+ * Coordinates the redux side effects
+ */
+const epic = (action$) => 
+  
+  action$
+    
+    // debounce so that we do not fire AJAX request on every keyup
+    .debounceTime(300)
+    
+    // react only to CHANGE_SEARCH_TXT actions
+    .filter(({ type, payload }) => type === CHANGE_SEARCH_TXT)
+
+    // convert action stream to the ajax start and end/fail actions
+    // we use switchMap that helps us convert the actions and at the same
+    // time it automatically unsubscribes the previous observables when new
+    // one comes in with values that allows us handle AJAX cancellation
+    .switchMap(action => concat$(
+
+      // For each ajax request we fire AJAC_SEARCH_START action
+      of$(ajaxSearchStart()),
+
+      // make an ajax request
+      ajax.getJSON(process.env.REACT_APP_GRAPHQL)
+
+        // fire AJAX_SEARCH_END action when successfully requested
+        .map(ajaxSearchEnd)
+
+        // fire AJAC_SEARCH_FAIL action if request is note successful
+        .catch(e => of$(ajaxSearchFail(e)))
+    ))
+
+
+export default epic;
---
+++ src/containers/SkyscrapersList/epic.test.js
@@ -0,0 +1,77 @@
+import { ReplaySubject } from 'rxjs/ReplaySubject';
+import xhrMock from 'utils/xhrMock';
+import {
+  changeSearchTxt,
+} from './actions';
+import {
+  AJAX_SEARCH_START,
+  AJAX_SEARCH_END,
+  AJAX_SEARCH_FAIL,
+} from './constants';
+import SkyscrapersListEpic from './epic';
+
+let action$;
+let epic$;
+let consumer;
+let subscribtion;
+
+jest.useFakeTimers();
+
+beforeEach(() => {
+  action$ = new ReplaySubject();
+  epic$ = SkyscrapersListEpic(action$);
+  consumer = jest.fn();
+  subscribtion = epic$.subscribe(consumer);
+});
+
+afterEach(() => {
+  subscribtion.unsubscribe();
+});
+
+test('SkyscrapersListEpic debounces actions and emits only the last one', () => {
+  action$.next(changeSearchTxt('foo'));
+  expect(consumer.mock.calls.length).toBe(0);
+  jest.runAllTimers();
+  console.log(consumer.mock.calls);
+  expect(consumer.mock.calls.length).toBeGreaterThan(0);
+});
+
+test('SkyscrapersListEpic does not react to every action', () => {
+  action$.next({ type: 'everyActionType' });
+  jest.runAllTimers();
+  expect(consumer.mock.calls.length).toBe(0);
+});
+
+test('SkyscrapersListEpic emits AJAX_SEARCH_START when recieves CHANGE_SEARCH_TXT action with not empty payload', () => {
+  action$.next(changeSearchTxt('foo'));
+  jest.runAllTimers();
+  expect(consumer.mock.calls.length).toBe(1);
+  expect(consumer.mock.calls[0][0].type).toBe(AJAX_SEARCH_START);
+});
+
+test('SkyscrapersListEpic makes xhr request when recieves CHANGE_SEARCH_TXT action with not empty payload', () => {
+  const fake = xhrMock();
+  action$.next(changeSearchTxt('foo'));
+  jest.runAllTimers();
+  expect(fake.requests.length).toBe(1);
+  fake.restore();
+});
+
+test('SkyscrapersListEpic emits AJAX_SEARCH_END action after successful ajax request with payload set to response body', () => {
+  const responseBody = { id: 13, message: 'hello world' };
+  const fake = xhrMock(200, JSON.stringify(responseBody));
+  action$.next(changeSearchTxt('foo'));
+  jest.runAllTimers();
+  expect(consumer.mock.calls[1][0].type).toBe(AJAX_SEARCH_END);
+  expect(consumer.mock.calls[1][0].payload).toEqual(responseBody);
+  fake.restore();
+});
+
+test('SkyscrapersListEpic emits AJAX_SEARCH_FAIL action on unsuccessful request with payload set to Error object', () => {
+  const fake = xhrMock(401);
+  action$.next(changeSearchTxt('foo'));
+  jest.runAllTimers();
+  expect(consumer.mock.calls[1][0].type).toBe(AJAX_SEARCH_FAIL);
+  expect(consumer.mock.calls[1][0].payload).toBeInstanceOf(Error);
+  fake.restore();
+});
