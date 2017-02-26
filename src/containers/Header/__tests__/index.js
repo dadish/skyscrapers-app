7b40d7777b73721151cb127c96bfba7e5446d4cd
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-26 17:02:36 +0600

    Implement Header menu.

--- src/containers/Header/__tests__/index.js
+++ src/containers/Header/__tests__/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { HeaderComponent } from '../';
 
+const props = {
+  currentPathname : '/',
+  goTo: () => {},
+}
+
 test('renders without crashing', () => {
-  shallow(<HeaderComponent />);
+  shallow(<HeaderComponent {...props} />);
 });
--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+import { selectPathname } from 'containers/App/selectors';
+import { push } from 'react-router-redux';
 import { Menu } from 'semantic-ui-react';
 
-export const HeaderComponent = () => {
+export const HeaderComponent = ({ currentPathname, goTo }) => {
   const { Item } = Menu;
   return (
     <Menu pointing secondary>
-      <Item>
-        Home
-      </Item>
+      <Item name="home" active={currentPathname === '/'} onClick={goTo('/')}>Home</Item>
+      <Item name="about" active={currentPathname === '/about'} onClick={goTo('/about')}>About</Item>
     </Menu>
   );
 };
 
-export default HeaderComponent;
+const mapStateToProps = createStructuredSelector({
+  currentPathname: selectPathname(),
+});
+
+const mapDispatchToProps = dispatch => ({
+  goTo: path => () => dispatch(push(path)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HeaderComponent);
