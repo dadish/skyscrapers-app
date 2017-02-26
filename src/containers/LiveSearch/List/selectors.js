8f60135e70b5207c68feceecb3e66e0224409918
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-26 06:49:24 +0600

    Remove LiveSearch container.

--- src/containers/LiveSearch/List/selectors.js
+++
@@ -1,7 +0,0 @@
-import { createSelector } from 'reselect';
-import { selectLiveSearch } from '../selectors';
-
-export const selectList = () => createSelector(
-  selectLiveSearch(),
-  liveSearch => liveSearch.get('list'),
-);
--- src/containers/LiveSearch/index.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import LiveSearch from './index';
-
-test('renders without crashing', () => {
-  shallow(<LiveSearch />);
-});
-
