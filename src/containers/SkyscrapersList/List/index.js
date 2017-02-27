86fef03d3127501527e37b22dfebda0b24492871
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-27 10:33:53 +0600

    Add loading.

--- src/containers/SkyscrapersList/List/index.js
+++ src/containers/SkyscrapersList/List/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import { Card, Loader} from 'semantic-ui-react';
 import { selectList } from './selectors';
-import { Card } from 'semantic-ui-react';
+import { selectLoading } from '../selectors';
 import Item from '../Item';
 
-export const ListComponent = ({ list }) => (
-  <Card.Group itemsPerRow={2}>
-    {list.map(item => <Item key={item.get('id')} skyscraper={item}/>)}
-  </Card.Group>
-);
+export const ListComponent = ({ list, loading }) => {
+  return (
+    <Card.Group itemsPerRow={2} style={{ minHeight: '200px' }}>
+      <Loader active={loading}>Loading...</Loader>
+      {list.map(item => <Item key={item.get('id')} skyscraper={item}/>)}
+    </Card.Group>
+  )
+};
 
 const mapStateToProps = createStructuredSelector({
   list: selectList(),
+  loading: selectLoading(),
 });
 
 export default connect(mapStateToProps)(ListComponent);
