06bfe2534e1a44e9c15b701d319a6033060f3e86
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-02-28 23:44:21 +0600

    Updates

---
+++ static/js/2.75de3bd5.chunk.js
@@ -0,0 +1,2 @@
+webpackJsonp([2],{269:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(1),a=n(l),r=function(){return a.default.createElement("div",{className:"pg-w"},a.default.createElement("h1",null,"404 Not Found"))};t.default=r}});
+//# sourceMappingURL=2.75de3bd5.chunk.js.map
