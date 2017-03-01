ed5d43809eb68e6e173868c32babd494ebfad564
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-01 11:39:21 +0600

    Decouple SearchEpic and move ajax logic to List.

--- src/containers/CitiesList/Search/epic.js
+++ src/containers/CitiesList/Search/epic.js
@@ -5,19 +5,13 @@ import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/merge';
 import 'rxjs/add/operator/switchMap';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { concat as concat$ } from 'rxjs/observable/concat';
-import { of as of$ } from 'rxjs/observable/of';
-import { ajax } from 'rxjs/observable/dom/ajax';
 import {
   CHANGE_SEARCH_TXT,
 } from '../constants';
 import {
-  ajaxSearchStart,
-  ajaxSearchEnd,
-  ajaxSearchFail,
   changeSearchTxt,
 } from '../actions';
-import { getQuery } from '../schema';
+import listSkyscrapers from '../List/epic';
 
 /**
  * CitiesList epic
@@ -41,27 +35,7 @@ const epic = (action$) =>
     // we use switchMap that helps us convert the actions and at the same
     // time it automatically unsubscribes the previous observables when new
     // one comes in with values that allows us handle AJAX cancellation
-    .switchMap(action => concat$(
-
-      // For each ajax request we fire AJAC_SEARCH_START action
-      of$(ajaxSearchStart()),
-
-      // make an ajax request
-      ajax({
-        url: process.env.REACT_APP_GRAPHQL_URL,
-        body: {
-          query: getQuery(),
-          variables: JSON.stringify({ selector: action.payload ? `title|body*=${action.payload}` : "" })
-        },
-        method: 'POST',
-      })
-
-        // fire AJAX_SEARCH_END action when successfully requested
-        .map((xhr) => ajaxSearchEnd(xhr.response))
-
-        // fire AJAC_SEARCH_FAIL action if request is note successful
-        .catch(e => of$(ajaxSearchFail(e)))
-    ))
+    .switchMap(({ payload }) => listSkyscrapers(payload))
 
 
 export default epic;
--- src/containers/CitiesList/epic.js
+++ src/containers/CitiesList/epic.js
@@ -1,6 +1,7 @@
 import { combineEpics } from 'redux-observable';
 import searchEpic from './Search/epic';
 
+
 const epic = combineEpics(searchEpic);
 
 export default epic;
