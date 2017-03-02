3512765c85098040853d6f7114f733e1a73bed65
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-02 11:56:31 +0600

    Add city prop for SkyscrapersItem.

--- src/containers/SkyscrapersList/Item/__tests__/index.js
+++ src/containers/SkyscrapersList/Item/__tests__/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { fromJS } from 'immutable';
 import { shallow } from 'enzyme';
-import ItemComponent from '../';
+import { ItemComponent } from '../';
 
 const props = {
   skyscraper: fromJS({})
--- src/containers/SkyscrapersList/Item/index.js
+++ src/containers/SkyscrapersList/Item/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { selectItem as selectCity } from 'containers/CitiesList/Item/selectors';
 import {
   Card,
   Image,
@@ -8,19 +10,28 @@ import {
 const {
   Header: CardHeader,
   Content: CardContent,
+  Meta: CardMeta,
 } = Card;
 
-export const ItemComponent = ({ skyscraper }) => (
-  <Card>
-    <Image fluid src="https://placehold.it/200/" alt={skyscraper.get('title')} />
-    <CardContent>
-      <CardHeader>{skyscraper.get('title')}</CardHeader>
-    </CardContent>
-    <CardContent extra>
-      <Statistic value={skyscraper.get('floors')} label="floors" size="mini"/>
-      <Statistic value={skyscraper.get('height')} label="height" size="mini"/>
-    </CardContent>
-  </Card>
-);
+export const ItemComponent = (props) => {
+  const { skyscraper, city } = props;
+  return (
+    <Card>
+      <Image fluid src="https://placehold.it/200/" alt={skyscraper.get('title')} />
+      <CardContent>
+        <CardHeader>{skyscraper.get('title')}</CardHeader>
+        {city && <CardMeta>{city.get('title')}</CardMeta>}
+      </CardContent>
+      <CardContent extra>
+        <Statistic value={skyscraper.get('floors')} label="floors" size="mini"/>
+        <Statistic value={skyscraper.get('height')} label="height" size="mini"/>
+      </CardContent>
+    </Card>
+  );
+};
 
-export default ItemComponent;
+const mapStateToProps = () => (state, { skyscraper }) => ({
+  city: selectCity(skyscraper.get('parentID'))(state)
+});
+
+export default connect(mapStateToProps)(ItemComponent);
