062726dbc4dd4d9359ac84fd4e30df3bc8912c3d
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-03 12:54:03 +0600

    Fix undefined switchMap method.

--- src/containers/Cities/List/epic.js
+++ src/containers/Cities/List/epic.js
@@ -1,5 +1,6 @@
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/catch';
 import { concat as concat$ } from 'rxjs/observable/concat';
 import { of as of$ } from 'rxjs/observable/of';
