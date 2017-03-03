ae1be16baaca03681dff6e24612fa95551550b45
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-03 13:06:07 +0600

    Add Cities link.

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -10,6 +10,7 @@ export const HeaderComponent = ({ currentPathname, goTo }) => {
   return (
     <Menu pointing secondary>
       <Item name="home" active={currentPathname === '/'} onClick={goTo('/')}>Home</Item>
+      <Item name="cities" active={currentPathname === '/cities'} onClick={goTo('/cities')}>Cities</Item>
       <Item name="about" active={currentPathname === '/about'} onClick={goTo('/about')}>About</Item>
     </Menu>
   );
