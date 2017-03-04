ad08bf311441ad725fff2e988603a7646709272c
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-04 15:46:50 +0600

    Test only the filterEpic.

--- src/containers/Skyscrapers/Filter/__tests__/epic.js
+++ src/containers/Skyscrapers/Filter/__tests__/epic.js
@@ -1,17 +1,11 @@
 import { fromJS } from 'immutable';
 import { ActionsObservable } from 'redux-observable';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
-import xhrMock from 'utils/xhrMock';
 import {
-  updateFilterKeyword,
-} from '../../actions';
-import { push } from 'react-router-redux';
-import {
-  AJAX_FETCH_START,
-  AJAX_FETCH_END,
-  AJAX_FETCH_FAIL,
-} from '../../constants';
+  change,
+} from 'redux-form';
 import SkyscrapersFilterEpic from '../epic';
+import ListEpic from '../../List/epic'
 
 let action$;
 let epic$;
@@ -27,75 +21,46 @@ const store = {
 
 jest.useFakeTimers();
 
+jest.mock('../../List/epic', () => jest.fn());
+
 beforeEach(() => {
   action$ = new ReplaySubject();
   epic$ = SkyscrapersFilterEpic(new ActionsObservable(action$), store);
   consumer = jest.fn();
   subscribtion = epic$.subscribe(consumer);
+  ListEpic.mockImplementation(() => {
+    const concat$ = require('rxjs/observable/concat').concat;
+    const of$ = require('rxjs/observable/of').of;
+    return concat$(of$({ type: 'Epic Test' }));
+  });
 });
 
 afterEach(() => {
   subscribtion.unsubscribe();
+  ListEpic.mockReset();
 });
 
-test('SkyscrapersFilterEpic debounces actions and emits only after 300ms passes', () => {
-  action$.next(updateFilterKeyword('foo'));
+test('SkyscrapersFilterEpic reacts only to redux-form CHANGE action', () => {
+  action$.next({ type: 'some action'});
+  jest.runAllTimers();
+  expect(consumer.mock.calls.length).toBe(0);
+  action$.next(change('foo', 'bar', 'baz'));
+  jest.runAllTimers();
+  expect(consumer.mock.calls.length).toBe(1);
+});
+
+test('SkyscrapersFilterEpic debounces the actions for 300ms', () => {
+  action$.next(change('foo', 'bar', 'baz'));
+  expect(consumer.mock.calls.length).toBe(0);
   jest.runTimersToTime(290);
   expect(consumer.mock.calls.length).toBe(0);
-});
-
-test('SkyscrapersFilterEpic does not react on randomActions', () => {
-  action$.next({ type: 'RandomAction', payload: 'foo' });
-  jest.runAllTimers();
-  expect(consumer.mock.calls.length).toBe(0);
-});
-
-test('SkyscrapersFilterEpic emits one AJAX_FETCH_START action on start', () => {
-  action$.next(updateFilterKeyword('foo'));
-  jest.runAllTimers();
+  jest.runTimersToTime(310);
   expect(consumer.mock.calls.length).toBe(1);
-  expect(consumer.mock.calls[0][0].type).toBe(AJAX_FETCH_START);
 });
 
-test('SkyscrapersFilterEpic emits AJAX_FETCH_START when recieves UPDATE_FILTER_KEYWORD action with not empty payload', () => {
-  action$.next(updateFilterKeyword('foo'));
+test('SkyscrapersFilterEpic switchMaps the stream to ListEpic', () => {
+  action$.next(change('foo', 'bar', 'baz'));
+  expect(ListEpic.mock.calls.length).toBe(0);
   jest.runAllTimers();
-  expect(consumer.mock.calls.length).toBe(1);
-  expect(consumer.mock.calls[0][0].type).toBe(AJAX_FETCH_START);
-});
-
-test('SkyscrapersFilterEpic makes xhr request when recieves UPDATE_FILTER_KEYWORD action with not empty payload', () => {
-  const fake = xhrMock();
-  action$.next(updateFilterKeyword('foo'));
-  jest.runAllTimers();
-  expect(fake.requests.length).toBe(1);
-  fake.restore();
-});
-
-test('SkyscrapersFilterEpic emits AJAX_FETCH_END action after successful ajax request with payload set to response body', () => {
-  const responseBody = { id: 13, message: 'hello world' };
-  const fake = xhrMock(200, JSON.stringify(responseBody));
-  action$.next(updateFilterKeyword('foo'));
-  jest.runAllTimers();
-  expect(consumer.mock.calls[1][0].type).toBe(AJAX_FETCH_END);
-  expect(consumer.mock.calls[1][0].payload).toEqual(responseBody);
-  fake.restore();
-});
-
-test('SkyscrapersFilterEpic always emits LOCATION_CHANGE action after AJAX_FETCH_END', () => {
-  const responseBody = { id: 13, message: 'hello world' };
-  const fake = xhrMock(200, JSON.stringify(responseBody));
-  action$.next(updateFilterKeyword('foo'));
-  jest.runAllTimers();
-  expect(consumer.mock.calls[2][0].type).toBe(push().type);
-  fake.restore();
-});
-
-test('SkyscrapersFilterEpic emits AJAX_FETCH_FAIL action on unsuccessful request with payload set to Error object', () => {
-  const fake = xhrMock(401);
-  action$.next(updateFilterKeyword('foo'));
-  jest.runAllTimers();
-  expect(consumer.mock.calls[1][0].type).toBe(AJAX_FETCH_FAIL);
-  expect(consumer.mock.calls[1][0].payload).toBeInstanceOf(Error);
-  fake.restore();
+  expect(ListEpic.mock.calls.length).toBe(1);
 });
--- src/containers/Skyscrapers/Filter/epic.js
+++ src/containers/Skyscrapers/Filter/epic.js
@@ -1,14 +1,11 @@
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/switchMap';
-import 'rxjs/add/operator/mergeMap';
-import 'rxjs/add/observable/from';
-import { push } from 'react-router-redux';
 import {
-  UPDATE_FILTER_KEYWORD,
-  AJAX_FETCH_END,
-} from '../constants';
-import { selectFilterKeyword } from '../selectors';
+  actionTypes,
+} from 'redux-form';
 import listEpic from '../List/epic';
+
+const { CHANGE } = actionTypes;
 
 /**
  * Skyscrapers epic
@@ -18,8 +15,8 @@ const epic = (action$, store) =>
   
   action$
     
-    // react only to UPDATE_FILTER_KEYWORD actions
-    .ofType(UPDATE_FILTER_KEYWORD)
+    // react only to CHANGE actions
+    .ofType(CHANGE)
 
     // debounce so that we do not fire AJAX request on every keyup
     .debounceTime(300)
@@ -28,18 +25,6 @@ const epic = (action$, store) =>
     // we use switchMap that helps us convert the actions and at the same
     // time it automatically unsubscribes the previous observables when new
     // one comes in with values that allows us handle AJAX cancellation
-    .switchMap(() => {
-      const keyword = selectFilterKeyword()(store.getState());
-      return listEpic({ keyword })
-        .mergeMap(action => {
-          const actionsList = [action];
-          if (action.type !== AJAX_FETCH_END) return actionsList;
-          let newLocation = '/';
-          if (keyword) newLocation += `?keyword=${keyword}`;
-          actionsList.push(push(newLocation));
-          return actionsList;
-        });
-      }
-    )
+    .switchMap(() => listEpic())
 
 export default epic;
