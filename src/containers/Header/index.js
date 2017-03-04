83889bd4d1fff328bbc4519292b545dc17345d97
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-04 21:52:53 +0600

    Make header bigger.

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -8,7 +8,7 @@ import { Menu } from 'semantic-ui-react';
 export const HeaderComponent = ({ currentPathname, goTo }) => {
   const { Item } = Menu;
   return (
-    <Menu pointing secondary>
+    <Menu pointing secondary size="massive">
       <Item name="home" active={currentPathname === '/'} onClick={goTo('/')}>Home</Item>
       <Item name="cities" active={currentPathname === '/cities'} onClick={goTo('/cities')}>Cities</Item>
       <Item name="about" active={currentPathname === '/about'} onClick={goTo('/about')}>About</Item>
