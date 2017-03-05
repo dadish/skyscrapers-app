6ff82683d5d2959475de305943b33cdd1b897855
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-05 19:55:24 +0600

    Fix the 1000+ bug.

--- src/containers/Skyscrapers/Filter/selectors.js
+++ src/containers/Skyscrapers/Filter/selectors.js
@@ -9,7 +9,7 @@ export const selectHeightOptions = () => () => [
   { key: '250-500', text: '250-500 ft.', value: '250-500' },
   { key: '500-750', text: '500-750 ft.', value: '500-750' },
   { key: '750-1000', text: '750-1000 ft.', value: '750-1000' },
-  { key: '1000+', text: '1000+ ft.', value: '1000+' },
+  { key: '1000+', text: '1000+ ft.', value: '1000-' },
 ];
 
 export const selectFloorsOptions = () => () => [
@@ -18,7 +18,7 @@ export const selectFloorsOptions = () => () => [
   { key: '20-40', text: '20-40 floors', value: '20-40' },
   { key: '40-60', text: '40-60 floors', value: '40-60' },
   { key: '60-80', text: '60-80 floors', value: '60-80' },
-  { key: '80+', text: '80+ floors', value: '80+' },
+  { key: '80+', text: '80+ floors', value: '80-' },
 ];
 
 export const selectYearOptions = () => () => {
@@ -61,22 +61,16 @@ const buildFilterSelector =   ({ keyword, cities, height, floors, year }) => {
 
   // height {string} --> min-max || min+ || ''
   if (height) {
-    if (height === '1000+') { // min+
-      selector += `, height>=1000`;
-    } else { // min-max
-      const [min, max] = height.split('-');
-      selector += `, height>=${min}, height<=${max}`;
-    }
+    const [min, max] = height.split('-');
+    if (min) selector += `, height>=${min}`;
+    if (max) selector += `, height<=${max}`;
   }
 
   // floors {string} -->  min-max || min+ || ''
   if (floors) {
-    if (floors === '80+') { // min+
-      selector += `, floors>=80`;
-    } else { // min-max
-      const [min, max] = floors.split('-');
-      selector += `, floors>=${min}, floors<=${max}`;
-    }
+    const [min, max] = floors.split('-');
+    if (min) selector += `, floors>=${min}`;
+    if (max) selector += `, floors<=${max}`;
   }
 
   // year {string} --> '1960' || ''
