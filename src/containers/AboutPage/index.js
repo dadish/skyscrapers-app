d1d46228548c91088ed9c1fa65c0da037e20c6d9
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-06 12:16:32 +0600

    Prettify About page.

--- src/containers/AboutPage/index.js
+++ src/containers/AboutPage/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Container } from 'semantic-ui-react';
 
 const About = () => (
-  <div className="pg-w">
+  <Container
+    text
+  >
     <h1>About Skyscrapers</h1>
     <p>
-      This is an SPA built for demostration purposes. The app demonstrates how you can create minimal viable product using <a href="https://processwire.com">ProcessWire</a> with <a href="https://github.com/dadish/processgraphql">ProcessGraphQL</a>.
+      This is an SPA built for demostration purposes. The app demonstrates how you can create minimal viable product using <a href="https://processwire.com" target="_blank">ProcessWire</a> with <a href="https://github.com/dadish/processgraphql" target="_blank">ProcessGraphQL</a>.
     </p>
-
-  </div>
+  </Container>
 );
 
 export default About;
