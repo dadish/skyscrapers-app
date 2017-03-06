768afd06d73e5be23db3f58ed2dca6f41dc49015
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-06 11:21:49 +0600

    Implement a selectFilteredList selector.

--- src/containers/Cities/List/__tests__/selectors.js
+++ src/containers/Cities/List/__tests__/selectors.js
@@ -1,14 +1,38 @@
 import { fromJS } from 'immutable';
-import { selectList } from '../selectors';
+import {
+  selectList,
+  selectFilteredList,
+} from '../selectors';
 
-const list = 'skyscrapers/Cities/List/list';
+const list = [
+  {id: '1234', title: 'Some text' },
+  {id: '1334', title: 'Some more text' },
+  {id: '1434', title: 'The world is mine' },
+  {id: '1534', title: 'People are awsome' },
+  {id: '1634', title: 'Testing is super fun!' },
+];
+
+let filterTxt = '';
 
 const state = fromJS({
   cities: {
     list,
+    filterTxt,
   },
 });
 
 test('selectList selects [`cities`, `list`]', () => {
-  expect(selectList()(state)).toBe(list);
+  expect(selectList()(state).toJS()).toEqual(list);
+});
+
+test('selectFilteredList returs a List of all city items if `filterTxt` is empty', () => {
+  filterTxt = '';
+  expect(selectFilteredList()(state).size).toBe(list.length);
+});
+
+test('selectFilteredList returns a List of city items whose title contains the `filterTxt`', () => {
+  const newState = state.setIn(['cities', 'filterTxt'], 'text');
+  const filteredList = selectFilteredList()(newState);
+  expect(filteredList.size).toBe(2);
+  expect(filteredList.get(0).get('id')).toBe('1234');
 });
--- src/containers/Cities/List/selectors.js
+++ src/containers/Cities/List/selectors.js
@@ -1,7 +1,16 @@
 import { createSelector } from 'reselect';
-import { selectCities } from '../selectors';
+import { selectCities, selectFilterTxt } from '../selectors';
 
 export const selectList = () => createSelector(
   selectCities(),
   cities => cities.get('list'),
+);
+
+export const selectFilteredList = () => createSelector(
+  selectList(),
+  selectFilterTxt(),
+  (list, filterTxt) => {
+    if (!filterTxt) return list;
+    return list.filter(item => item.get('title').indexOf(filterTxt) !== -1);
+  }
 );
