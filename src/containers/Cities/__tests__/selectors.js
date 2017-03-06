8615ecbec6ea21a0c8de7d28e66e5ebc0285facc
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-06 11:04:57 +0600

    Add selectFilterTxt selector.

--- src/containers/Cities/__tests__/selectors.js
+++ src/containers/Cities/__tests__/selectors.js
@@ -1,18 +1,24 @@
 import { fromJS } from 'immutable';
 import {
   selectCities,
+  selectFilterTxt,
 } from '../selectors';
 
 const loading = 'cities/Cities/loading';
+const filterTxt = 'cities/Cities/filterTxt';
 
 const cities = {
-  loading
+  filterTxt,
 };
 
 const state = fromJS({
   cities,
-})
+});
 
 test('selectCities() selects [`cities`]', () => {
   expect(selectCities()(state).toJS()).toEqual(cities);
+});
+
+test('selectFilterTxt() selects [`cities`, `filterTxt`]', () => {
+  expect(selectFilterTxt()(state)).toEqual(filterTxt);
 });
--- src/containers/Cities/selectors.js
+++ src/containers/Cities/selectors.js
@@ -1,3 +1,8 @@
 import { createSelector } from 'reselect';
 
 export const selectCities = () => state => state.get('cities');
+
+export const selectFilterTxt = () => createSelector(
+  selectCities(),
+  cities => cities.get('filterTxt'),
+);
