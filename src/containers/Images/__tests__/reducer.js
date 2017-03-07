001309a08c134e04087d86a8b1219bc352a399cc
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-07 18:02:27 +0600

    Implement a reducer logic for handling image titles response

---
+++ src/containers/Images/__tests__/reducer.js
@@ -0,0 +1,38 @@
+import reducer, { initialState } from '../reducer'
+import * as a from '../actions'
+
+const firstPageId = 338534;
+const secondPageId = 338535;
+const firstTitle = 'File:Bank-of-America-Plaza-Atlanta-24802603.jpg';
+const fourthTitle = 'File:191 Peachtree Street Building.JPG';
+
+const payload = [
+  {
+    pageid: firstPageId,
+    ns: 0,
+    images: [
+      { ns: 6, title: firstTitle },
+      { ns: 6, title: "File:Bank of America Atlanta 2.jpg" },
+      { ns: 6, title: "File:Bank of America Corporate Center.jpg" },
+    ]
+  },
+  {
+    pageid: secondPageId,
+    ns: 0,
+    images: [
+      { ns: 6, title: fourthTitle },
+      { ns: 6, title: "File:191 Peachtree Tower.jpg" },
+      { ns: 6, title: "File:Commons-logo.svg" },
+    ],
+  },
+];
+
+test('reducer returns the same state for unknown actions', () => {
+  expect(reducer(initialState, { type: 'uneskfxn e'})).toBe(initialState)
+})
+
+test('reducer appends the items in the payload to the state on AJAX_IMG_TTL_END action', () => {
+  const newState = reducer(initialState, a.ajaxImageTitleFetchEnd(payload))
+  expect(newState.get(0)).toEqual({ pageId: firstPageId, title: firstTitle })
+  expect(newState.get(3)).toEqual({ pageId: secondPageId, title: fourthTitle })
+})
--- src/containers/Images/reducer.js
+++ src/containers/Images/reducer.js
@@ -1,11 +1,24 @@
-import { fromJS } from 'immutable';
+import { List } from 'immutable';
+import * as c from './constants';
 
-const initialState = fromJS({
-  list: []
-})
+export const initialState = new List();
 
 const reducer = (state = initialState, action) => {
-  return state;
+  const { type, payload } = action;
+  switch (type) {
+
+    case c.AJAX_IMG_TTL_FETCH_END:
+      return payload.reduce((state, wikipediaPage) => {
+        const pageId = wikipediaPage.pageid;
+        return wikipediaPage.images.reduce((state, { title }) => {
+          return state.push({ pageId, title })
+        }, state);
+      }, state);
+
+    default:
+      return state;
+
+  }
 };
 
 export default reducer;
