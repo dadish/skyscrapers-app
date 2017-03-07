766abf99fc134391581b3a02cac417ba5f42b0f7
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-07 20:14:22 +0600

    Emit  AJAX_GET_IMAGES action after each list fetch

--- src/containers/Skyscrapers/List/epic.js
+++ src/containers/Skyscrapers/List/epic.js
@@ -1,10 +1,12 @@
-import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/filter';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/switchMap';
 import { concat as concat$ } from 'rxjs/observable/concat';
 import { of as of$ } from 'rxjs/observable/of';
 import { ajax } from 'rxjs/observable/dom/ajax';
+import { ajaxGetImages } from 'containers/Images/actions';
+import { NAME } from '../constants';
 import {
   ajaxFetchStart,
   ajaxFetchEnd,
@@ -29,7 +31,10 @@ const listEpic = (selector = "") =>
     })
 
       // fire AJAX_FETCH_END action when successfully requested
-      .map((xhr) => ajaxFetchEnd(xhr.response))
+      .mergeMap((xhr) => concat$(
+        of$(ajaxFetchEnd(xhr.response)),
+        of$(ajaxGetImages(NAME)),
+        ))
 
       // fire AJAC_FILTER_FAIL action if request is note successful
       .catch(e => of$(ajaxFetchFail(e)))
