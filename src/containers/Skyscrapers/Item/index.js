6f23f94540070f5acc204a44e93a677ffe94ee54
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-09 12:29:50 +0600

    Add thumbnail from wikipedia

--- src/containers/Skyscrapers/Item/index.js
+++ src/containers/Skyscrapers/Item/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { selectItem as selectCity } from 'containers/Cities/Item/selectors';
+import { selectThumbnailForPage } from 'containers/Images/selectors';
 import {
   Card,
-  Image,
   Statistic,
 } from 'semantic-ui-react';
+import CenteredImage from 'components/CenteredImage';
 import { push } from 'react-router-redux';
 
 const {
@@ -14,12 +15,12 @@ const {
 } = Card;
 
 export const ItemComponent = (props) => {
-  const { skyscraper, city, goToCityPage } = props;
+  const { skyscraper, city, goToCityPage, thumb } = props;
   const height = skyscraper.get('height') || null;
   const floors = skyscraper.get('floors') || null;
   return (
     <Card>
-      <Image fluid src="https://placehold.it/200/" alt={skyscraper.get('title')} />
+      <CenteredImage src={thumb} alt={skyscraper.get('title')} />
       <CardContent>
         <CardHeader>{skyscraper.get('title')}</CardHeader>
         {city && <a href={`/?cities=${city.get('id')}`} onClick={goToCityPage(city.get('id'))}>{city.get('title')}</a>}
@@ -33,7 +34,8 @@ export const ItemComponent = (props) => {
 };
 
 const mapStateToProps = () => (state, { skyscraper }) => ({
-  city: selectCity(skyscraper.get('parentID'))(state)
+  city: selectCity(skyscraper.get('parentID'))(state),
+  thumb: selectThumbnailForPage(skyscraper.get('wikipedia_id'))(state),
 });
 
 const mapDispatchToProps = dispatch => ({
