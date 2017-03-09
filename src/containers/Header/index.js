b5c81ba736de377fd9f57139ae54899a590f7606
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-10 00:10:57 +0600

    Add depth

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -17,6 +17,7 @@ export const HeaderComponent = ({ currentPathname, goTo }) => {
       style={{
         backgroundColor: '#fff',
         borderColor: '#fff',
+        boxShadow: '0px 5px 5px -3px rgba(0, 0, 0, 0.2), 0px 8px 10px 1px rgba(0, 0, 0, 0.14), 0px 3px 14px 2px rgba(0, 0, 0, 0.12)'
       }}
     >
       <Item name="home" active={currentPathname === '/'} onClick={goTo('/')}>Home</Item>
