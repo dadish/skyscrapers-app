7b902e0a398681a9e2bf2ff914ed51b7676ea2e1
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-16 19:25:31 +0600

    Set the columns to two

--- src/containers/Skyscrapers/List/index.js
+++ src/containers/Skyscrapers/List/index.js
@@ -11,7 +11,7 @@ const { Column } = Grid;
 export const ListComponent = ({ list, loading }) => {
   const loadingStr = list.size ? 'Fetching more...' : 'Fetching...';
   return (
-    <Grid columns={3}>
+    <Grid columns={2}>
       {list.size ? list.map(item => (
         <Column key={item.get('id')}>
           <Item skyscraper={item}/>
