577dc75557a9158d3a8cc46f7f6e38dfa9453b9a
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-16 19:27:24 +0600

    Add github link

--- src/containers/Skyscrapers/Summary/index.js
+++ src/containers/Skyscrapers/Summary/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import Segment from 'components/Segment';
+import { Icon } from 'semantic-ui-react';
 import { createStructuredSelector } from 'reselect';
 import { selectList } from '../List/selectors';
 import { selectListTotal } from '../selectors';
@@ -8,8 +9,13 @@ import './style.css';
 
 export const SummaryComponent = ({ total, list }) => (
   <Segment raise="2" className="smry-w">
-    <span className="smry-lbl">Total:</span> <span className="smry-val">{total}</span>
-    <span className="smry-lbl">Showing:</span> <span className="smry-val">{list.size}</span>
+    <div className="smry-info">
+      <span className="smry-lbl">Total:</span> <span className="smry-val">{total}</span>
+      <span className="smry-lbl">Showing:</span> <span className="smry-val">{list.size}</span>
+    </div>
+    <a className="smry-source" href="https://github.com/dadish/skyscrapers-app" target="_blank">
+      <Icon name="github" size="large"/>
+    </a>
   </Segment>
 );
 
