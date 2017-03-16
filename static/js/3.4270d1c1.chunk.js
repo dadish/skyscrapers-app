310bf84038f02ccf3cc582eb6a06885f04028d48
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-16 19:30:05 +0600

    Updates

--- static/js/3.4270d1c1.chunk.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([3],{311:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(1),a=n(l),r=function(){return a.default.createElement("div",{className:"pg-w"},a.default.createElement("h1",null,"404 Not Found"))};t.default=r}});
-//# sourceMappingURL=3.4270d1c1.chunk.js.map
