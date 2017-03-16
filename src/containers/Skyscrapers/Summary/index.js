2472b33c9fec1831936d2c74d96e1c5652c33b5c
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-16 18:34:56 +0600

    Style symmary

--- src/containers/Skyscrapers/Summary/index.js
+++ src/containers/Skyscrapers/Summary/index.js
@@ -4,9 +4,13 @@ import Segment from 'components/Segment';
 import { createStructuredSelector } from 'reselect';
 import { selectList } from '../List/selectors';
 import { selectListTotal } from '../selectors';
+import './style.css';
 
 export const SummaryComponent = ({ total, list }) => (
-  <Segment raise="2">Total: {total}, showing: {list.size}</Segment>
+  <Segment raise="2" className="smry-w">
+    <span className="smry-lbl">Total:</span> <span className="smry-val">{total}</span>
+    <span className="smry-lbl">Showing:</span> <span className="smry-val">{list.size}</span>
+  </Segment>
 );
 
 const mapStateToProps = createStructuredSelector({
