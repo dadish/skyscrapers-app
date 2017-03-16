ce80c553cf27d5b6940eadfb7b463b33f9634707
Nurguly Ashyrov <nurguly.ashyrov@gmail.com>
2017-03-16 12:11:50 +0600

    Show list summary

--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 import { selectLocationPathname } from 'containers/App/selectors';
+import { selectListTotal } from 'containers/Skyscrapers/selectors';
+import { selectList } from 'containers/Skyscrapers/List/selectors';
 import { push } from 'react-router-redux';
 import { Menu } from 'semantic-ui-react';
 import { elevation8 } from 'components/styles';
 
-export const HeaderComponent = ({ currentPathname, goTo }) => {
+export const HeaderComponent = ({ currentPathname, total, list, goTo }) => {
   const { Item } = Menu;
   return (
     <Menu
@@ -22,12 +24,16 @@ export const HeaderComponent = ({ currentPathname, goTo }) => {
     >
       <Item name="home" active={currentPathname === '/'} onClick={goTo('/')}>Home</Item>
       <Item name="about" active={currentPathname === '/about'} onClick={goTo('/about')}>About</Item>
+      <Item name="summary" position="right"> Total: {total}, showing: {list.size}</Item>
+      <Item position="right"></Item>
     </Menu>
   );
 };
 
 const mapStateToProps = createStructuredSelector({
   currentPathname: selectLocationPathname(),
+  total: selectListTotal(),
+  list: selectList(),
 });
 
 const mapDispatchToProps = dispatch => ({
